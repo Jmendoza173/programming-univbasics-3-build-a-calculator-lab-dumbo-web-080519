ea070f3c910800c8742977bbaa554d4226eff430
Joel Mendoza <joelmendza173@gmail.com>
2019-07-13 08:49:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,9 @@
 def addition(num1, num2)
-  add(num1, num2)
+  num1+num2
 end
 
 def subtraction(num1, num2)
-  sub(num1, num2)
+  num1-num2
 end
 
 def division(num1, num2)
@@ -19,5 +19,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  math.sqrt(num)
+  Math.sqrt(num)
 end
